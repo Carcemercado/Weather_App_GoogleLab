b889e187306abf9d931e99b32da5826e93b8dd11
Glitch (googlecodelabs-your-first-pwapp-sdrbmi1wlv) <none>
2020-02-12 14:34:54 +0000

    🌯🍩 Checkpoint ./public/service-worker.js:11077105/806

--- public/service-worker.js
+++ public/service-worker.js
@@ -57,9 +57,9 @@ self.addEventListener('activate', (evt) => {
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
   // CODELAB: Add fetch event handler here.
-    if (evt.request.mode !== 'navigate') {
+  if (evt.request.mode !== 'navigate') {
   // Not a page navigation, bail.
-    return;
+  return;
   }
   evt.respondWith(
       fetch(evt.request)
