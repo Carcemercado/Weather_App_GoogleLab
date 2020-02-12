40a3b536131ff5ccfaab03589cdaf6e8faf94e69
Glitch (googlecodelabs-your-first-pwapp-sdrbmi1wlv) <none>
2020-02-12 16:59:56 +0000

    🍡🌂 Checkpoint ./public/scripts/app.js:11077105/607

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -163,9 +163,22 @@ function getForecastFromNetwork(coords) {
  */
 function getForecastFromCache(coords) {
   // CODELAB: Add code to get weather forecast from the caches object.
-
+  if (!('caches' in window)) {
+  return null;
+  }
+  const url = `${window.location.origin}/forecast/${coords}`;
+  return caches.match(url)
+      .then((response) => {
+        if (response) {
+          return response.json();
+        }
+        return null;
+      })
+      .catch((err) => {
+        console.error('Error getting data from cache', err);
+        return null;
+      });
 }
-
 /**
  * Get's the HTML element for the weather forecast, or clones the template
  * and adds it to the DOM if we're adding a new item.
@@ -198,7 +211,10 @@ function updateData() {
     const location = weatherApp.selectedLocations[key];
     const card = getForecastCard(location);
     // CODELAB: Add code to call getForecastFromCache
-
+    getForecastFromCache(location.geo)
+    .then((forecast) => {
+      renderForecast(card, forecast);
+    });
     // Get the forecast data from the network.
     getForecastFromNetwork(location.geo)
         .then((forecast) => {
